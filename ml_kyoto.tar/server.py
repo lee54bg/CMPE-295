e4e6a4af75e5d35fcad5815315d2f7f743c6b0e7
unknown <lee078bg@yahoo.com>
2019-03-19 09:51:45 -0700

    k8s

---
+++ ml_kyoto.tar/server.py
@@ -0,0 +1,30 @@
+# Import libraries
+import numpy as np
+from flask import Flask, request, jsonify
+import json
+import pickle
+
+app = Flask(__name__)
+
+# Load the model
+model = pickle.load(open('/app/model.pkl','rb'))
+
+@app.route('/api',methods=['POST'])
+def predict():
+    # Get the data from the POST request.
+    data = request.get_json(force=True)
+    
+    # Make prediction using model loaded from disk as per the data.
+    load_data = [data['exp']]
+    
+    # Take our loaded data and perform the prediction
+    prediction = model.predict(load_data)
+    
+    print(prediction)
+    # Take the first value of prediction
+    output = np.array(prediction).tolist()
+    
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=7000, debug=True)
