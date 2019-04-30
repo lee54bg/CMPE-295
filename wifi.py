13d5b5efb430f6cc9e36821f83ed741418dd9b1a
SEED <lee078bg@yahoo.com>
2019-04-29 18:14:08 -0700

    Wi-Fi done

--- wifi.py
+++ wifi.py
@@ -1,38 +1,53 @@
 import sys
 import os
 from scapy.all import *
+import numpy as np
+import requests
 
-iface = sys.argv[1]
+try:
+    import Queue as queue
+except ImportError:
+    import queue
+
+data_to_send = queue.Queue()
+
+def basic_function():
+    print("Rest API Started")
+    
+    url = 'http://10.0.2.30:5000/api'
+
+    while True:
+        if data_to_send.empty():
+            continue
+        else:
+            data = data_to_send.get()
+
+            # Format the data into a list though the data is already in a list
+            # data = np.array(data).tolist()
+            r = requests.post(url,json={'exp':data})
+
+            print(r.json())
 
 def sniff_80211(packet):
-    packet.show()
-    # if Dot11 in packet:
-    #     print("True")
+    
+    if Dot11 in packet:
+        temp_list = [packet[Dot11].ID, len(packet[Dot11])]
+        data_to_send.put(temp_list)
+
+iface = sys.argv[1]
 
 if len(sys.argv) == 1:
     print("Usage: python wifi.py {Interface}")
     sys.exit()
 else:
     try:
+        import threading
+  
+        t1 = threading.Thread(target=basic_function)
+        t1.start()
+
         sniff(prn=sniff_80211, iface=iface)
     
     except KeyboardInterrupt:
         print("\nTerminating Program...")
 
-# packets = rdpcap("wifi.pcap")
-
-# for packet in packets:
-# 	# packet[Dot11Elt].info
-# 	# if packet.haslayer(RadioTap):
-# 	# 	try:
-# 	# 		print("Radio Tap: {}".format(packet[RadioTap].Flags))
-# 	# 	except:
-# 	# 		print("Error")
-# 	if packet.haslayer(Dot11):
-# 		# print("Subtype:  {}".format(packet[Dot11].subtype))
-# 		fcfield = packet[Dot11].FCfield
-# 		print(format(fcfield, '08b'))
-		
-# 		# print("Flags:  {}".format(packet[Dot11].FCfield))
-# 		# print("Duration:  {}".format(packet[Dot11].ID))
-# 		# print("Length:  {}".format(len(packet[Dot11])))
